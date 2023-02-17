b9a3597d523c743df2b392a4717500329448a6ef
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2023-02-17 18:59:33 -0300

    fix added dashboard function in components

--- src/pages/board1.jsx
+++ src/pages/board1.jsx
@@ -1,58 +1,10 @@
 import React from "react";
 import { Box, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Chart from "chart.js/auto";
+import Dash from "../components/Dash";
 
 const Board1 = () => {
-  const [chartData, setChartData] = useState(null);
-  const getRandomData = () => {
-    const data = [];
-    for (let i = 0; i < 7; i++) {
-      data.push(Math.floor(Math.random() * 50) + 10);
-    }
-    return data;
-  };
-  useEffect(() => {
-    const ctx = document.getElementById("myChart");
-    const chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday",
-        ],
-        datasets: [
-          {
-            label: "Sales",
-            data: getRandomData(),
-            backgroundColor: "rgba(54, 162, 235, 0.2)",
-            borderColor: "rgba(54, 162, 235, 1)",
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        },
-      },
-    });
-    setChartData(chart);
-    return ()=>{
-        chart.destroy()
-    }
-  }, []);
-
+  
   const Navigate = useNavigate();
 
   const HandleButtonClick = () => {
@@ -61,7 +13,7 @@ const Board1 = () => {
   return (
     <>
       <div>
-        <canvas id="myChart" width="400" height="400"></canvas>
+        <Dash />
       </div>
       <Box>
         <h1> pasaste </h1>
